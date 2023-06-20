25a723982709430c8d39d15b99799df06a3ec392
hyosoek <tennfin1@gmail.com>
2023-06-21 01:13:31 +0900

    First commit in WebBasic

---
+++ index.js
@@ -0,0 +1,26 @@
+init()
+
+function init(){
+    createTab()
+}
+
+function createTab() {
+    for(var index =0;index <4; index++){
+        var tmpBtn = document.createElement("input")
+        tmpBtn.type = "button"
+        tmpBtn.value = "week" + (index+1).toString()
+        tmpBtn.classList.add("pageButton")
+        tmpBtn.addEventListener('click', function(index){
+            var htmalNameSet = ['week1/studywork1.html','week2/youtubeMain.html','week3/baseball.html','week4/dragPuzzle.html']
+            location.href = htmalNameSet[index]
+        })
+        document.getElementById("container").appendChild(tmpBtn)
+    }
+}
+
+function buttonEvent(index) {
+    
+    return function(event) {
+        location.href = htmalNameSet[index]
+      }
+}
